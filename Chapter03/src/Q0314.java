89397b5f646d11ac6c8c35742d611b2ab9346762
taek00n <kimth0526@naver.com>
2023-07-24 23:01:39 +0900

    Day 230724-2

---
+++ Chapter03/src/Q0314.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Q0314 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        // 우선순위 큐 구현
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if (first_abs == second_abs) {
+                return o1 > o2 ? 1 : -1;
+            } else {
+                return first_abs - second_abs;
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int request = Integer.parseInt(bf.readLine());
+            if (request  == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.add(request);
+            }
+        }
+    }
+}
